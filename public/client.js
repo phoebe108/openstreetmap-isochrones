f1ad38be837f097e5e535550d99e9237f2876e32
Phoebe Merritt <pmerritt@spatialdev.com>
2016-02-18 11:42:20 -0800

    osrm isochrone endpoint

--- public/client.js
+++ public/client.js
@@ -1,13 +1,32 @@
 var app = angular.module('app', ['ui.bootstrap']);
 
 app.controller('MapCtrl', function($scope, $http) {
-  var map = L.map('map').setView([-1.265236,36.806609], 13);
 
+  var map = L.map('map').setView([-1.265236,36.806609], 13);
+  $scope.coords = '';
 
   // add basemap to the leaflet map
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       maxZoom: 18
   }).addTo(map);
+
+
+  // retrieve isochrone geometries using clicked map location as starting point
+  map.on('click', function(e) {
+
+    $scope.coords = [e.latlng.lat,e.latlng.lng];
+
+    // request to OSRM service for json isochrone lines
+    $http.post('/osrm', $scope.coords)
+        .success(function(response) {
+
+            // add isochrone json to the leaflet map
+            $scope.isochroneLayer = L.geoJson(response, {style: $scope.isoLineStyle}).addTo(map);
+        })
+        .error(function(response) {
+            console.log(response);
+        });
+  });
 
 });
--- server.js
+++ server.js
@@ -6,11 +6,39 @@ var app = express(); // application instance
 
 var bodyParser = require('body-parser');
 
+var concave = require('turf-concave');
+var isochrone = require('osrm-isochrone');
+
 app.use(express.static('public')); //serve static file in the public directory
 app.use(bodyParser.json());
 
 // route url
 app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.post('/osrm', function(req, res, next) {
+    var location = [req.body[1], req.body[0]] || null;
+
+    // calculate one isochrone line geom using coordinates(lat/long) and time(seconds)
+    function getIsochrones(coords, time) {
+        var iso = {};
+        var options = {
+            resolution: 25, // sample resolution
+            maxspeed: 4, // in 'unit'/hour
+            unit: 'miles', // 'miles' or 'kilometers'
+            network: __dirname + '/nairobi_foot/nairobi_kenya.osrm' // prebuild dc osrm network file
+        };
+
+        isochrone(coords, time, options, function(err, drivetime) {
+            if(err) throw err;
+            // a geojson line
+            res.json(drivetime);
+            res.end();
+        });
+    }
+
+    getIsochrones(location, 2400); // 40min
 });
 
 // starts a server and listens on port 3001
