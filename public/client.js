8fc334b4eebe1b6981aa087a1d327ed4b0c403e8
Phoebe Merritt <pmerritt@spatialdev.com>
2016-02-18 11:53:02 -0800

    otp isochrone polygon styling

--- public/client.js
+++ public/client.js
@@ -26,6 +26,39 @@ app.controller('MapCtrl', function($scope, $http) {
       }
   };
 
+  // get color depending on travel time value
+  function getColor(t) {
+      return t == "Isochrone 600 sec" ? 'red' :
+      t == "Isochrone 1200 sec"  ? '#E32C21' :
+      t == "Isochrone 1800 sec"  ? '#F6642E' :
+      t == "Isochrone 2400 sec"  ? '#FD9C44' :
+      t == "Isochrone 3000 sec"   ? '#FECC5C' :
+      t == "Isochrone 3600 sec"   ? '#FEF29C' :
+      '#ffffe5';
+  }
+
+  // get opacity depending on travel time value
+  function getFillOpacity(t2) {
+      return t2 == "Isochrone 600 sec" ? 0.40 :
+      t2 == "Isochrone 1200 sec" ? 0.40 :
+      t2 == "Isochrone 1800 sec" ? 0.40 :
+      t2 == "Isochrone 2400 sec" ? 0.40 :
+      t2 == "Isochrone 3000 sec"  ? 0.45 :
+      t2 == "Isochrone 3600 sec"  ? 0.50 :
+      1;
+  }
+
+  // generate the way travel time isochrones will look
+  function getStyle(feature) {
+      return {
+          fillOpacity: getFillOpacity(feature.properties.name),
+          color: 'white',
+          weight: 1,
+          opacity: 0.7,
+          fillColor: getColor(feature.properties.name)
+      };
+  }
+
   // rework json into Leaflet-recognizable GeoJSON
   function getProperGeojsonFormat(data) {
       $scope.isoGeojson = {"type": "FeatureCollection", "features": []};
@@ -51,7 +84,7 @@ app.controller('MapCtrl', function($scope, $http) {
     $scope.coords = [e.latlng.lat,e.latlng.lng];
 
     if ($scope.chosenTool == 'OTP') {
-      $scope.url = 'http://localhost:8080/otp/routers/default/isochrone?&fromPlace=' + $scope.coords + '&date=2015/01/09&time=12:00:00&mode=WALK&walkSpeed=3&cutoffSec=600&cutoffSec=1200&cutoffSec=2400&callback=JSON_CALLBACK';
+      $scope.url = 'http://localhost:8080/otp/routers/default/isochrone?&fromPlace=' + $scope.coords + '&date=2015/01/09&time=12:00:00&mode=WALK&walkSpeed=4&cutoffSec=600&cutoffSec=1200&cutoffSec=2400&callback=JSON_CALLBACK';
 
       // request to OTP service for json isochrone polygons
       $http.jsonp($scope.url)
@@ -59,7 +92,7 @@ app.controller('MapCtrl', function($scope, $http) {
 
               // add the isochrone json to the leaflet map
               $scope.properGeoJSON = getProperGeojsonFormat(response);
-              $scope.isochroneLayer = L.geoJson($scope.properGeoJSON).addTo(map);
+              $scope.isochroneLayer = L.geoJson($scope.properGeoJSON, {style: getStyle}).addTo(map);
           })
           .error(function(response) {
               console.log(response);
